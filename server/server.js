b0da4501e813ce8364f1e553255a3370a0909dc3
aaron shivers <aaorn@moonmail.org>
2019-09-02 12:59:58 -0500

    added compression

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const express = require('express')
+const compression = require('compression')
 
 const app = express()
 const publicPath = path.join(__dirname, '..', 'dist')
 const port = process.env.PORT || 3000
 
+app.use(compression())
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => res.sendFile(path.join(publicPath, 'index.html')))
